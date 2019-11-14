a9bbbfb66d03d76c31c125f584cd6ce3b34f4484
khalid el masnaoui <khalidelmasnaoui1@gmail.com>
2019-11-14 16:10:09 +0100

    test7

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ function execute(channel) {
                 country.textContent = "country : " + response.result.items[0].snippet.country;
                 view.textContent = "view number : " + response.result.items[0].statistics.viewCount;
                 video_number.textContent = "video number : " + response.result.items[0].statistics.videoCount;
-                channelID = response.result.items[0].contentDetails.relatedPlayLists.uploads;
+                channelID = response.result.items[0].contentDetails.relatedPlaylists.uploads;
             },
             function(err) { console.error("Execute error", err); });
 }
