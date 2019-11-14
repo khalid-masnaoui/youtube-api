a2943e7d1aaf28e4458a371482764b7e5d638040
khalid el masnaoui <khalidelmasnaoui1@gmail.com>
2019-11-14 12:50:09 +0100

    6

--- main.js
+++ main.js
@@ -28,7 +28,7 @@
           })
           .then(function(response) {
                   // Handle the results here (response.result has the parsed body).
-                  console.log("Response", response);
+                  console.log("Response", response, "hey");
                   document.getElementById("h3").textContent = response;
               },
               function(err) { console.error("Execute error", err); });
