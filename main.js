60fcd2c17ba3dbd4baed357d1af22dfaf13cf1b5
khalid el masnaoui <khalidelmasnaoui1@gmail.com>
2019-11-14 15:03:12 +0100

    test2

--- main.js
+++ main.js
@@ -11,21 +11,34 @@
   const country = document.querySelector("#country");
   const view = document.querySelector("#view");
   const video_number = document.querySelector("#video_number");
+  const hello = false;
 
 
 
   function authenticate() {
       return gapi.auth2.getAuthInstance()
           .signIn({ scope: "https://www.googleapis.com/auth/youtube.readonly" })
-          .then(function() { console.log("Sign-in successful"); },
-              function(err) { console.error("Error signing in", err); });
+          .then(function() {
+                  console.log("Sign-in successful");
+                  hello == true
+              },
+              function(err) {
+                  console.error("Error signing in", err);
+                  hello == false
+              });
   }
 
   function loadClient() {
       gapi.client.setApiKey(API_KEY);
       return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-          .then(function() { console.log("GAPI client loaded for API"); },
-              function(err) { console.error("Error loading GAPI client for API", err); });
+          .then(function() {
+                  console.log("GAPI client loaded for API");
+                  hello == true
+              },
+              function(err) {
+                  console.error("Error loading GAPI client for API", err);
+                  hello == false
+              });
   }
   // Make sure the client is loaded and sign-in is complete before calling this method.
   function execute(channel) {
@@ -48,6 +61,30 @@
       gapi.auth2.init({ client_id: CLIENT_ID });
   });
 
+  // log in
+
+  document.querySelector(".autorized").addEventListener("click", logIn);
+
+  function logIn(e) {
+      authenticate().then(loadClient);
+      if (hello == true) {
+          e.target.style.display = "none";
+          document.querySelector(".execute").style.display = "block";
+          document.querySelector("form").style.display = "block";
+      }
+  };
+
+  //log Out 
+  document.querySelector(".execute").addEventListener("click", logOut);
+
+  function logOut(e) {
+
+
+      e.target.style.display = "none";
+      document.querySelector(".autorized").style.display = "block";
+      document.querySelector("form").style.display = "none";
+
+  };
 
   //get channel data
   document.querySelector("form").addEventListener("submit", getData);
